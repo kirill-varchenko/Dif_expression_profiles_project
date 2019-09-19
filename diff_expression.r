75940e505c577d318b0b5e8a152390314633a3b5
Kirill Varchenko <kirill.varchenko@gmail.com>
2019-09-19 11:52:40 +0300

    Suppot for local KEGG data added.

--- diff_expression.r
+++ diff_expression.r
@@ -2,19 +2,59 @@ library(KEGGgraph)
 library(KEGG.db)
 library(KEGGREST)
 library(gage)
-library("readxl")
-library(dplyr)
+library(readxl)
+# library(dplyr)
 library(ggplot2)
 
-data(kegg.gs)
-data(kegg.gs.dise)
+### GLOBAL ###
+
+use_local_kegg <- TRUE
+local_kegg_dir <- 'kegg_data'
+
+min_genes <- 5 # min number of genes in the pathway to cutoff
+
+### FUNCTIONS ###
+
+# parsing KEGG graphs
+# using local files from local_kegg_dir when use_local_kegg is true
+# tmpfiles otherwise
+load_hsa <- function(hsa_name) {
+  filehsa <- ifelse(use_local_kegg, paste0(local_kegg_dir, '/hsa', hsa_name, '.xml'), tempfile())
+  if ( !(use_local_kegg & file.exists(filehsa)) ) {
+    downloadable <- tryCatch(
+      expr = {
+        retrieveKGML(hsa_name, organism="hsa", destfile = filehsa, method="internal", quiet=TRUE)
+        print(paste(hsa_name, 'downloaded to:', filehsa))
+        TRUE
+      },
+      error = function(e) {
+        print(paste0('Unable to download: hsa', hsa_name))
+        FALSE
+      },
+      warning = function(e) {
+        print(paste0('Unable to download: hsa', hsa_name))
+        FALSE
+      }
+    )
+    if (!downloadable) {
+      if (file.exists(filehsa)) {
+        file.remove(filehsa)
+      }
+      return(NA)
+    }
+  } else {
+    print(filehsa)
+  }
+  mapkG <- parseKGML2Graph(filehsa, expandGenes=T)
+  return(mapkG)
+}
 
 # main function that finds all descendants for each gene 
 gene_iterator_forward <- function(goal_gene, 
                                   unique_symbol_names,
                                   forward_edges_list,
                                   diff_expressed_gene_symb){
-
+  
   flag_visited <- rep(0,length(unique_symbol_names))
   names(flag_visited) <- unique_symbol_names
   
@@ -26,14 +66,14 @@ gene_iterator_forward <- function(goal_gene,
   while (counter <= length(all_children)) {
     current_child <- all_children[counter]
     children_of_this_child <<- forward_edges_list[[current_child]]
-
+    
     for_children_to_add <<- names(which(flag_visited[children_of_this_child] == 0))
-
+    
     all_children <- c(all_children, for_children_to_add)
     
     flag_visited[for_children_to_add] <- 1
     counter <- counter + 1
-
+    
   }
   #intersection between all gene descendants and differentially expressed ones
   all_children <- intersect(all_children, diff_expressed_gene_symb) 
@@ -41,21 +81,22 @@ gene_iterator_forward <- function(goal_gene,
   return (all_children) #returns only differentially expressed descendants for each gene
 }
 
+### START HERE ###
+
+data(kegg.gs)
+data(kegg.gs.dise)
+
 # the ability to choose between two datasets, depending on p_value
 # you can download tast dataframe and read it from your computer (with your own path)
-diff_expressed <- read_excel("C:/Users/admin/Desktop/gse_16357_001.xlsx") 
+diff_expressed <- read_excel('gse_16357_001.xlsx') 
 diff_expressed <- diff_expressed$gse16357_entrez
-
 #diff_expressed <- read_excel("C:/Users/admin/Desktop/experiment.xlsx")
 #diff_expressed <- diff_expressed$gse16357
-
 diff_expressed <- as.vector(diff_expressed)
-
 diff_expressed <- diff_expressed[-grep("/+", diff_expressed)]
 
 all_KEGG_pathways <- append(kegg.gs, kegg.gs.dise) #browse pathways from KEGG database
 list_intersected_genes <- lapply(all_KEGG_pathways, function(x) intersect(diff_expressed, x)) #intersection between genes in datasets and pathway genes from prev step
-
 short_list <- list_intersected_genes[lengths(list_intersected_genes) >= 5] #cutoff by min 5 genes in the pathway
 pathids <- substr(names(short_list), start=4, stop=8) #extracting IDs
 
@@ -65,16 +106,7 @@ pathids <- substr(names(short_list), start=4, stop=8) #extracting IDs
 ################
 
 #downloading pathways that contains genes from dataset
-graph_list <- lapply(pathids, function(hsa_name){
-  mapkG <- try(
-  {
-    tmp <- tempfile()
-    print(hsa_name)
-    retrieveKGML(hsa_name, organism="hsa", destfile = tmp, method="internal", quiet=TRUE)
-    mapkG <- parseKGML2Graph(tmp,expandGenes=T)
-    mapkG
-  })  
-})
+graph_list <- lapply(pathids, load_hsa)
 ok_graphs <- sapply(graph_list, function(x) typeof(x) == "S4")
 graph_list <- graph_list[ok_graphs]
 #mega_graph <- mergeKEGGgraphs(graph_list) #merge all pathways in one big graph
@@ -158,7 +190,6 @@ gene <- c("DDB2", "MAP2K", "GRB2", "GADD45G", "ROCK1", "CALML6")
 freq <- c(9,9,10,11,11,12)
 frec_anz <- data.frame(Gene_name = gene, n_path = freq)
 
- ggplot(frec_anz ,aes(reorder(Gene_name, -n_path),n_path, fill = Gene_name)) + 
-+     geom_bar(stat = "identity") + ggtitle("Most abundant genes") 
+ggplot(frec_anz ,aes(reorder(Gene_name, -n_path),n_path, fill = Gene_name)) + geom_bar(stat = "identity") + ggtitle("Most abundant genes") 
 
 
